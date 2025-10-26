1352c1986baf0e11c2a100575f2a1529acdd0135
Natan Lucas <167147473+natanlucas18@users.noreply.github.com>
2025-10-26 16:49:15 -0300

    adiciona configurações do jwt

---
+++ src/auth/config/jwt.config.ts
@@ -0,0 +1,10 @@
+import { registerAs } from '@nestjs/config';
+export default registerAs('jwt', () => {
+  return {
+    secret: String(process.env.JWT_SECRET),
+    audience: String(process.env.JWT_SECRET),
+    issuer: String(process.env.JWT_SECRET),
+    expiresIn: Number(process.env.JWT_SECRET),
+    jwtRefreshTtl: Number(process.env.JWT_SECRET),
+  };
+});
