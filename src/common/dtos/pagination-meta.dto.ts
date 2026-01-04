cf83d37ca5f12daf13c73475af9cb6acdf3a8e51
natanlucas18 <natanlucaslg1839@gmail.com>
2026-01-03 21:57:00 -0300

    feat: adiciona PaginationMeta para padronização de retorno de query

---
+++ src/common/dtos/pagination-meta.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PaginationMetaDto {
+  @ApiProperty({ example: 1 })
+  page: number;
+
+  @ApiProperty({ example: 10 })
+  limit: number;
+
+  @ApiProperty({ example: 95 })
+  totalItems: number;
+
+  @ApiProperty({ example: 10 })
+  totalPages: number;
+
+  @ApiProperty({ example: true })
+  hasNextPage: boolean;
+
+  @ApiProperty({ example: false })
+  hasPreviousPage: boolean;
+}
